7810fd5b198af4844e2749d450a51b7f40f2ec70
Evgeny <Swordelf@mail.ru>
2017-10-26 10:46:00 +0300

    finised writing ht02-5

--- ht02/5/test.c
+++ ht02/5/test.c
@@ -1,17 +1,27 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <dirent.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+
+ssize_t getcwd2(int fd, char *buf, size_t size);
+
+enum
+{
+    BUFF_SIZE = 100
+};
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-    struct stat my_stat;
-    ino_t my_ino;
-    stat("hey1", &my_stat);
-    my_ino = my_stat.st_ino;
-    stat("hey2", &my_stat);
-    printf("%d %d\n", my_ino, my_stat.st_ino);
+    char *buff = malloc(BUFF_SIZE);
+    DIR *my_dir = opendir(argv[1]);
+    ssize_t result = getcwd2(dirfd(my_dir), buff, BUFF_SIZE);
+    closedir(my_dir);
+    puts(buff);
+    printf("return value = %d\n", result);
+    free(buff);
     return 0;
 }
