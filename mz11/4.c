d3e82c1140354b7f91f3db27a9191212cf819e69
Swordelf <Swordelf@mail.ru>
2017-11-24 13:49:48 +0300

    mz11-4 finished ??

--- mz11/4.c
+++ mz11/4.c
@@ -29,7 +29,7 @@ work(int myid, int fd_rd, int fd_wr, int max_num, sigset_t set_prev)
         fprintf(p_out, "%d ", getpid());
         fflush(p_out);
     } else if (myid == 2) {
-        fprintf(p_out, "1");
+        fprintf(p_out, "1 ");
         fflush(p_out);
     }
 
@@ -44,12 +44,15 @@ work(int myid, int fd_rd, int fd_wr, int max_num, sigset_t set_prev)
         int sc_res;
         sc_res = fscanf(p_in, "%d", &x);
         if (sc_res != 1 || x == max_num) {
+            fprintf(p_out, "%d ", max_num);
+            fflush(p_out);
             break;
         }
         printf("%d %d\n", myid, x);
         fflush(stdout);
         fprintf(p_out, "%d ", x + 1);
         fflush(p_out);
+        kill(opid, SIGUSR1);
     }
     fclose(p_in);
     fclose(p_out);
@@ -68,22 +71,24 @@ main(int argc, char *argv[])
     pipe(fd);
     pid_t ch1, ch2;
 
+    int max_num = strtol(argv[1], NULL, 0);
     if ((ch1 = fork()) == 0) {
-        work(1, fd[0], fd[1], strtol(argv[1], NULL, 0), set_prev);
-        exit(0);
+        work(1, fd[0], fd[1], max_num, set_prev);
+        _exit(0);
     }
     if ((ch2 = fork()) == 0) {
-        work(2, fd[0], fd[1], strtol(argv[1], NULL, 0), set_prev);
-        exit(0);
+        work(2, fd[0], fd[1], max_num, set_prev);
+        _exit(0);
     }
     close(fd[0]);
     FILE *p_out = fdopen(fd[1], "w");
     fprintf(p_out, "%d ", ch2);
-    fflush(p_out);
     fclose(p_out);
     kill(ch1, SIGUSR1);
+
     wait(NULL);
     wait(NULL);
+
     printf("Done\n");
     return 0;
 }
