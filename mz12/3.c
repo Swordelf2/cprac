f8bb8d1eb7151f6a92d10e5e8f8b7f3cef8b8e3c
Swordelf <Swordelf@mail.ru>
2017-12-07 13:42:04 +0300

    startd mz12-3

--- mz12/3.c
+++ mz12/3.c
@@ -7,11 +7,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Returns x % y (mathematical)
+int
+rem(int x, int y)
+{
+    int rem = x % y;
+    if (rem < 0) {
+        return y + rem;
+    } else {
+        return rem;
+    }
+}
+
 int
 main(int argc, char *argv[])
 {
     int count = strtol(argv[1], NULL, 0);
-    setbuf(stdout, NULL);
+    setbuf(stdin, NULL);
     key_t key = ftok(argv[0], 0);
 
     int semid = semget(key, count, 0600 | IPC_CREAT);
@@ -21,8 +33,34 @@ main(int argc, char *argv[])
     }
     for (int p = 0; p < count; ++p) {
         if (fork() == 0) {
-            semop(semid, (struct sembuf[]) {
-                    {
+            // Down(p)
+            if ((semop(semid, 
+                    (struct sembuf[]) {{.sem_num = p, .sem_op = -1}},
+                    1)) == -1) {
+                _exit(0);
+            }
 
+            int x;
+            int sc_res;
+            sc_res = scanf("%d", &x);
+            if (sc_res != 1) {
+                // Abort
+                semctl(semid, 0, IPC_RMID);
+                _exit(0);
+            }
+            printf("%d %d\n", p, x);
+            fflush(stdout);
+
+            //Up(x mod P)
+            semop(semid,
+                    (struct sembuf[]) {{.sem_num = rem(x, count), .sem_op = +1}},
+                    1);
+        }
+    }
+
+    semop(semid, (struct sembuf[]) {{.sem_num = 0, .sem_op = +1}}, 1);
+
+    while (wait(NULL) > 0);
+                               
     return 0;
 }
